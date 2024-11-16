bdebf8d7694ac25e83e638e118b6438085f77344
David MacMahon <davidm@astro.berkeley.edu>
2024-11-15 20:46:03 -0800

    Parameterize DirWalker AbstractChannel type
    This is in preparation for distributed dir walking.

--- src/DirWalkers.jl
+++ src/DirWalkers.jl
@@ -2,17 +2,41 @@ module DirWalkers
 
 export DirWalker, start_dirwalker
 
-struct DirWalker{T}
-    dirq::Channel{String}
-    fileq::Channel{String}
-    outq::Channel{Union{Nothing,T}}
+#=
+struct DirWalkerOld{T,C<:Channel}
+    dirq::C{String}
+    fileq::C{String}
+    outq::C{Union{Nothing,T}}
+end
+=#
+
+struct DirWalker{T,CS<:AbstractChannel{String},CT<:AbstractChannel{Union{Nothing,T}}}
+    dirq::CS
+    fileq::CS
+    outq::CT
+end
+
+#=
+function DirWalker{T,C}(d,f,o) where {T, C<:AbstractChannel}
+    DirWalker{T,C{String},C{Union{Nothing,T}}}(d,f,o)
+end
+=#
+
+function DirWalker{T}(::Type{C}; dqsize=10_000, fqsize=10_000, oqsize=10_000) where {T, C}
+    dirq = C{String}(dqsize)
+    fileq = C{String}(fqsize)
+    outq = C{Union{Nothing,T}}(oqsize)
+    DirWalker{T,C{String},C{Union{Nothing,T}}}(dirq, fileq, outq)
 end
 
 function DirWalker{T}(; dqsize=10_000, fqsize=10_000, oqsize=10_000) where T
+    DirWalker{T}(Channel; dqsize, fqsize, oqsize)
+#=
     dirq = Channel{String}(dqsize)
     fileq = Channel{String}(fqsize)
     outq = Channel{Union{Nothing,T}}(oqsize)
-    DirWalker{T}(dirq, fileq, outq)
+    DirWalker{T,Channel{String},Channel{Union{Nothing,String}}}(dirq, fileq, outq)
+=#
 end
 
 DirWalker(; dqsize=10_000, fqsize=10_000, oqsize=10_000) = DirWalker{Any}(; dqsize, fqsize, oqsize)
@@ -93,6 +117,14 @@ catch ex
 end
 end
 
+function start_dirtasks(filepred, dw::DirWalker, ndirtasks)
+    [errormonitor(Threads.@spawn _process_dirs(filepred, dw)) for _=1:ndirtasks]
+end
+
+function start_filetasks(filefunc, dw::DirWalker, nfiletasks, args...; kwargs...)
+    [errormonitor(Threads.@spawn _process_files(filefunc, dw, args...; kwargs...)) for _=1:nfiletasks]
+end
+
 function start_dirwalker(filefunc, dw::DirWalker, topdirs, args...;
     filepred=_->true, ndirtasks=1, nfiletasks=1, kwargs...
 )
@@ -101,11 +133,10 @@ function start_dirwalker(filefunc, dw::DirWalker, topdirs, args...;
 
     runner = Threads.@spawn begin
         # Start dir tasks
-
-        dirtasks = [errormonitor(Threads.@spawn _process_dirs(filepred, dw)) for _=1:ndirtasks]
+        dirtasks = start_dirtasks(filepred, dw::DirWalker, ndirtasks)
 
         # Start file tasks
-        filetasks = [errormonitor(Threads.@spawn _process_files(filefunc, dw, args...; kwargs...)) for _=1:nfiletasks]
+        filetasks = start_filetasks(filefunc, dw::DirWalker, nfiletasks, args...; kwargs...)
 
         # Populate dw.dirq.  It is important to do this after starting tasks to
         # avoid blocking before tasks are started.
